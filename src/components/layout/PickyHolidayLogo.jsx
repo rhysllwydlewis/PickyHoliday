9544ac0173482e1e21ffb5769f51acc81738015c
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-06-06 17:56:29 +0100

    Fix logo wordmark spacing and domain gap

--- src/components/layout/PickyHolidayLogo.jsx
+++ src/components/layout/PickyHolidayLogo.jsx
@@ -3,19 +3,19 @@ import { scrollToId } from '../../app/appConstants.js';
 import './PickyHolidayLogo.css';
 import './PickyHolidayLogoFixes.css';
 
-const staticPlaneTransform = 'translate(586 21) rotate(-14)';
+const staticPlaneTransform = 'translate(596 21) rotate(-14)';
 const introSettleDelay = 4200;
 
 const layout = {
   pickyX: 26,
-  hX: 226,
-  sunX: 308,
+  hX: 246,
+  sunX: 326,
   sunY: 92,
-  lidayX: 340,
-  domainX: 538,
+  lidayX: 358,
+  domainX: 518,
 };
 
-const flightPath = 'M274 55 C336 25 420 18 486 27 C528 33 558 30 586 21';
+const flightPath = 'M286 55 C348 25 432 18 498 27 C540 33 568 30 596 21';
 let firstNavbarLogoRender = true;
 
 function prefersReducedMotion() {
@@ -37,7 +37,7 @@ function handleLogoClick() {
     scrollToId('top');
     window.scrollTo({ top: 0, behavior: 'smooth' });
   } else {
-    window.location.assign('/');
+    window.location.href = '/';
   }
 }
 
@@ -100,7 +100,7 @@ export function Logo({ footer = false }) {
               <stop offset="58%" stopColor="#fff" stopOpacity="0" />
               <stop offset="100%" stopColor="#fff" stopOpacity="0" />
             </linearGradient>
-            <mask id={flightMaskId} maskUnits="userSpaceOnUse" x="262" y="0" width="350" height="90">
+            <mask id={flightMaskId} maskUnits="userSpaceOnUse" x="274" y="0" width="350" height="90">
               <path className="brand-logo-flight-reveal" d={flightPath} pathLength="240" fill="none" stroke="#fff" strokeWidth="16" strokeLinecap="round" />
             </mask>
             <clipPath id={shimmerClipId}>
